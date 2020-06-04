96b4abf4833d6cc747b9e4048958207d909b602b
Paul Gao <gaozihou@uw.edu>
2020-06-03 23:57:33 -0700

    update bsg_dfi_to_fifo to support clock gating

--- toplevels/bsg_ac_black_parrot_quad_core_rc3/v/bsg_dfi_to_fifo.v
+++ toplevels/bsg_ac_black_parrot_quad_core_rc3/v/bsg_dfi_to_fifo.v
@@ -45,13 +45,34 @@ module bsg_dfi_to_fifo
   );
   
   // error signal
+/*
   logic wr_error_r, wr_error_n;
   logic cmd_error_r, cmd_error_n;
   logic rd_error_r, rd_error_n;
   assign fifo_error_o = wr_error_r | cmd_error_r | rd_error_r;
-
+*/
 
   // handle write data
+  
+  wire w_async_fifo_full_lo;
+  bsg_async_fifo
+ #(.lg_size_p(3)
+  ,.width_p  (2*dq_data_width_p+2*dq_group_lp)
+  ) w_async_fifo
+  (.w_clk_i  (dfi_clk_1x_i)
+  ,.w_reset_i(dfi_rst_i)
+  ,.w_enq_i  (dfi_wrdata_en_i)
+  ,.w_data_i ({dfi_wrdata_i, dfi_wrdata_mask_i})
+  ,.w_full_o (w_async_fifo_full_lo)
+
+  ,.r_clk_i  (fifo_clk_i)
+  ,.r_reset_i(fifo_reset_i)
+  ,.r_deq_i  (fifo_wr_v_o & fifo_wr_ready_i)
+  ,.r_data_o (fifo_wr_data_o)
+  ,.r_valid_o(fifo_wr_v_o)
+  );
+  
+/*
   assign fifo_wr_data_o = {dfi_wrdata_i, dfi_wrdata_mask_i};
   
   logic fifo_wr_v_lo;
@@ -88,9 +109,29 @@ module bsg_dfi_to_fifo
           end
       end
   end
-
+*/
 
   // handle cmd
+
+  wire cmd_async_fifo_full_lo;
+  bsg_async_fifo
+ #(.lg_size_p(3)
+  ,.width_p  (3+16+7)
+  ) cmd_async_fifo
+  (.w_clk_i  (dfi_clk_1x_i)
+  ,.w_reset_i(dfi_rst_i)
+  ,.w_enq_i  (~dfi_cs_n_i)
+  ,.w_data_i ({dfi_bank_i, dfi_address_i, dfi_cke_i, dfi_cs_n_i, dfi_ras_n_i, dfi_cas_n_i, dfi_we_n_i, dfi_reset_n_i, dfi_odt_i})
+  ,.w_full_o (cmd_async_fifo_full_lo)
+
+  ,.r_clk_i  (fifo_clk_i)
+  ,.r_reset_i(fifo_reset_i)
+  ,.r_deq_i  (fifo_cmd_v_o & fifo_cmd_ready_i)
+  ,.r_data_o (fifo_cmd_data_o)
+  ,.r_valid_o(fifo_cmd_v_o)
+  );
+
+/*
   assign fifo_cmd_data_o = {dfi_bank_i, dfi_address_i, dfi_cke_i, dfi_cs_n_i, dfi_ras_n_i, dfi_cas_n_i, dfi_we_n_i, dfi_reset_n_i, dfi_odt_i};
   
   logic fifo_cmd_v_lo;
@@ -127,7 +168,7 @@ module bsg_dfi_to_fifo
           end
       end
   end
-
+*/
 
   // handle read data
   bsg_dff_chain 
@@ -139,11 +180,45 @@ module bsg_dfi_to_fifo
   ,.data_o      (dfi_rddata_valid_o)
   );
   
-  assign dfi_rddata_o = fifo_rd_data_i;
+  logic rd_toggle_r;
+  bsg_dff_reset_en #(.width_p(1)) rd_toggle_r_dff
+  (.clk_i  (dfi_clk_1x_i)
+  ,.reset_i(dfi_rst_i)
+  ,.data_i (~rd_toggle_r)
+  ,.en_i   (dfi_rddata_valid_o)
+  ,.data_o (rd_toggle_r)
+  );
   
+  logic rd_toggle_rr;
+  bsg_dff #(.width_p(1)) rd_toggle_rr_dff
+  (.clk_i  (fifo_clk_i)
+  ,.data_i (rd_toggle_r)
+  ,.data_o (rd_toggle_rr)
+  );
+  
+  assign dfi_rddata_o = fifo_rd_data_i;
+  assign fifo_rd_yumi_o = rd_toggle_r ^ rd_toggle_rr;
+  
+  logic dfi_rddata_valid_r;
+  bsg_dff_chain 
+ #(.width_p     (1)
+  ,.num_stages_p(1)
+  ) rdvalid_r_dff
+  (.clk_i       (fifo_clk_i)
+  ,.data_i      (dfi_rddata_valid_o)
+  ,.data_o      (dfi_rddata_valid_r)
+  );
+  
+  bsg_dff #(.width_p(1)) error_dff
+  (.clk_i  (fifo_clk_i)
+  ,.data_i (dfi_rddata_valid_r & ~fifo_rd_v_i)
+  ,.data_o (fifo_error_o)
+  );
+  
+/*  
   logic fifo_rd_yumi_lo;
   assign fifo_rd_yumi_o = fifo_rd_yumi_lo;
-  
+
   logic [7:0] rd_count_r, rd_count_n;
   always_ff @(posedge fifo_clk_i)
   begin
@@ -175,5 +250,6 @@ module bsg_dfi_to_fifo
           end
       end
   end
+*/
 
 endmodule
