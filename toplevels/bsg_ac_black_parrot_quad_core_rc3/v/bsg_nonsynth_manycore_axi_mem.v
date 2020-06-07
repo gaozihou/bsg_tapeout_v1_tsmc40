818d926d71bace74b4729159a2a3a855312b8f07
Huwan Peng <hwpeng20@gmail.com>
2020-06-06 23:20:56 -0700

    fix read burst address bug in axi_mem

--- toplevels/bsg_ac_black_parrot_quad_core_rc3/v/bsg_nonsynth_manycore_axi_mem.v
+++ toplevels/bsg_ac_black_parrot_quad_core_rc3/v/bsg_nonsynth_manycore_axi_mem.v
@@ -176,6 +176,10 @@ module bsg_nonsynth_manycore_axi_mem
         rd_state_n = ((rd_burst_r == axi_burst_len_p-1) & axi_rready_i)
           ? RD_WAIT_ADDR
           : RD_SEND_DATA;
+
+        araddr_n = axi_rready_i
+          ? araddr_r + (1 << `BSG_SAFE_CLOG2(axi_data_width_p>>3))
+          : araddr_r;
       
       end
     endcase
